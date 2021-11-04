bec76b6ddaf8ae797e903cac4847fabf3f56c4b1
jigalovd <jigalovd@gmail.com>
2021-11-04 17:04:12 +0200

    optional function parameters

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Dima", age = 28, gender = "male";
+const sayHi = (name, age, gender) => console.log(`Hello ${name}, you are ${age} ages old and you are ${gender}`);
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const name = "Dima",
+      age = 28,
+      gender = "male"
+
+const sayHi = (name, age, gender?) => console.log(`Hello ${name}, you are ${age} ages old and you are ${gender}`);
+
+sayHi(name, age);
+export {}
