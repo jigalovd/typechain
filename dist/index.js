7b7a73abcf668afcca9e71e7d5bf389c9fa0087b
jigalovd <jigalovd@gmail.com>
2021-11-04 17:20:44 +0200

    input types and return types

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "Dima", age = 28, gender = "male";
 const sayHi = (name, age, gender) => console.log(`Hello ${name}, you are ${age} ages old and you are ${gender}`);
-sayHi(name, age);
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const name = "Dima",
       age = 28,
       gender = "male"
 
-const sayHi = (name, age, gender?) => console.log(`Hello ${name}, you are ${age} ages old and you are ${gender}`);
+const sayHi = (name: string, age: number, gender?: string): void => console.log(`Hello ${name}, you are ${age} ages old and you are ${gender}`);
 
-sayHi(name, age);
+sayHi(name, age, gender);
 export {}
