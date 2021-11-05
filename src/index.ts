f9297d31460941803c1d1d81a9e0cad7460349cb
jigalovd <jigalovd@gmail.com>
2021-11-05 19:53:59 +0200

    public-private fields

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
-const name = "Dima",
-      age = 28,
-      gender = "male"
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  private pGender: string;
+  
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.pGender = "private value"
+  }
 
-const sayHi = (name: string, age: number, gender?: string): void => console.log(`Hello ${name}, you are ${age} ages old and you are ${gender}`);
+  sayPrivate() {
+    console.log(this.pGender);
+  }
+}
 
-sayHi(name, age, gender);
+const person = new Human("Dima", 38, "male");
+
+const sayHi = (person: Human): void => console.log(`Hello ${person.name}, you are ${person.age} ages old and you are ${person.gender}`);
+
+sayHi(person);
 export {}
